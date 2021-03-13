609d4166d84d8c86da6550e963eef32115df83f5
priks <gautamprikshit1@gmail.com>
2021-03-13 11:58:06 +0530

    1st Commit

---
+++ src/components/Todo.vue
@@ -0,0 +1,40 @@
+<template>
+  <input type="text" v-model="newTodo" @keypress.enter="addTodo"/>
+  <button @click="addTodo">Add Todo</button>
+  <div v-for="todo in todoList" :key="todo.id">
+    <p>{{ todo.title }}</p>
+    <button @click="deleteTodo(todo)">Delete Todo</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todoList: [
+        {
+          id: 1,
+          title: "Watch Constantine",
+        },
+        {
+          id: 2,
+          title: "Make a Todo List",
+        },
+      ],
+      newTodo: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todoList.push({ id: this.todoList.length + 1, title: this.newTodo });
+      this.newTodo = "";
+    },
+    deleteTodo(closeTodo) {
+      this.todoList.splice(this.todoList.indexOf(closeTodo), 1);
+    }
+  },
+};
+</script>
+
+<style>
+</style>
